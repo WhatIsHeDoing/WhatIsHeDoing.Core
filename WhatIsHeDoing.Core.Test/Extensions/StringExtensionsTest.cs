87106529b1df0448455e21d07fdcc9e30be17ee8
Darren Hickling <darren@hickling.is>
2015-07-13 22:20:52 +0100

    Adding generic Parse and TryParse string extension methods and tests

---
+++ WhatIsHeDoing.Core.Test/Extensions/StringExtensionsTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhatIsHeDoing.Core.Extensions;
+
+namespace WhatIsHeDoing.Core.Test.Extensions
+{
+    public static class StringExtensionsTest
+    {
+        [TestClass]
+        public class Parse
+        {
+            [TestMethod]
+            public void Valid()
+            {
+                var i = "123".Parse<int>();
+                Assert.AreEqual(123, i);
+
+                var d = "12.3".Parse<double>();
+                Assert.AreEqual(12.3, d);
+
+                var de = "12.3".Parse<decimal>();
+                Assert.AreEqual(12.3m, de);
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(TypeLoadException))]
+            public void Invalid()
+            {
+                "123".Parse<string>();
+            }
+        }
+
+        [TestClass]
+        public class TryParse
+        {
+            [TestMethod]
+            public void Valid()
+            {
+                int i;
+                var result = "123".TryParse<int>(out i);
+                Assert.IsTrue(result);
+                Assert.AreEqual(123, i);
+
+                double d;
+                result = "12.3".TryParse<double>(out d);
+                Assert.IsTrue(result);
+                Assert.AreEqual(12.3, d);
+
+                decimal dec;
+                result = "123".TryParse<decimal>(out dec);
+                Assert.IsTrue(result);
+                Assert.AreEqual(123m, dec);
+            }
+
+            [TestMethod]
+            public void Invalid()
+            {
+                string s;
+                var result = "123".TryParse<string>(out s);
+                Assert.IsFalse(result);
+                Assert.IsNull(s);
+            }
+        }
+    }
+}
---
+++ WhatIsHeDoing.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection;
+
+namespace WhatIsHeDoing.Core.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for string.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Invoke a Parse method from the TResult type on the value.
+        /// </summary>
+        /// <typeparam name="TResult">Containing the parse method</typeparam>
+        /// <param name="value">To parse</param>
+        /// <returns>Parsed value</returns>
+        /// <exception cref="TypeLoadException">
+        /// Thrown if TResult does not have a Parse method.
+        /// </exception>
+        public static TResult Parse<TResult>(this string value)
+        {
+            TResult result;
+            
+            if (!value.TryParse<TResult>(out result))
+            {
+                throw new TypeLoadException("Cannot find Parse method");
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Tries to invoke a Parse method from the TResult type on the value.
+        /// </summary>
+        /// <typeparam name="TResult">Containing the parse method</typeparam>
+        /// <param name="value">To parse</param>
+        /// <param name="result">Parsed value or default on failure</param>
+        /// <returns>Success</returns>
+        public static bool TryParse<TResult>
+            (this string value, out TResult result)
+        {
+            var methodInfo = typeof(TResult).GetMethod
+                ("Parse", new Type[] { typeof(string) });
+
+            if (methodInfo == null)
+            {
+                result = default(TResult);
+                return false;
+            }
+
+            result = (TResult) methodInfo.Invoke(null, new object[] { value });
+            return true;
+        }
+    }
+}
